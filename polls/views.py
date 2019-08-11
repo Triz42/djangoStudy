5551494a59cb68f7e02adafcd4d171829ffc1d1b
Beatriz Teles Castro <beatrizteles868@gmail.com>
2019-08-10 23:00:28 -0300

    Finalizando parte 1 do tutorial django

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. YOU'RE AT THE POLLS INDEX!"
+    return HttpResponse("Hello, world. YOU'RE AT THE POLLS INDEX!")
